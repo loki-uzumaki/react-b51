323f0cce172c624473069004028240ea5a269f3d
loki-uzumaki <lokeshkumararaja@gmail.com>
2023-10-16 10:12:10 +0530

    adding multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-function App() {
-  const now = new Date();
-
-  //simple addition 
-  const a = 10;
-  const b = 20;
+function Hello() {
   return (
-    <div> <p>Hello World, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p></div>
+    <p>Hello World</p>
+  )
+}
+function App() {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
   )
 
 }
