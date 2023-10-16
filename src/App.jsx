d2a2f15fe62bbad21fca21beda321773c499e56c
loki-uzumaki <lokeshkumararaja@gmail.com>
2023-10-16 10:29:54 +0530

    passing data as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-function Hello() {
+function Hello(props) {
   return (
-    <p>Hello World</p>
+    <p>Hello {props.username}</p>
   )
 }
 function App() {
+  let user1 = 'lokesh';
+  let user2 = 'luffy';
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello username={user1} />
+      <Hello username={user2} />
     </div>
   )
 
