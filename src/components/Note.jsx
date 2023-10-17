e657239cbda4a37808ed6f8651ef783f51892423
loki-uzumaki <lokeshkumararaja@gmail.com>
2023-10-17 11:50:39 +0530

    conditional rendering ---first way

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function Note({ note }) {
+    if (note.important) {
+        return (
+            <li>{note.content}★</li>
+        )
+    }
     return (
         <li>{note.content}</li>
     )
